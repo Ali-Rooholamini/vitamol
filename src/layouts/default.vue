5316138f5910f497d769ced5d46e09115e7d393c
Ali-Rooholamini <ali.rooholamini.dev@gmail.com>
2023-12-28 23:02:50 +0330

    feat: add default-layout_upside-gradient under menu section

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <TheHeader />
+    <div
+      class="layout_upside-gradient"
+      :class="{ 'landing-gradient': routeName }"
+    ></div>
     <slot />
     <TheFooter />
   </div>
@@ -16,5 +20,39 @@ export default {
     TheHeader,
     TheFooter,
   },
+
+  data() {
+    return {
+      routeName: this.$route.name === "index",
+    };
+  },
+
+  watch: {
+    $route(to) {
+      this.routeName = to.name === "index";
+    },
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.layout_upside-gradient {
+  height: 160px;
+  width: 100%;
+  background: linear-gradient(
+    180deg,
+    rgba(55, 84, 131, 1) 0%,
+    rgba(219, 224, 233, 1) 60%,
+    rgba(255, 255, 255, 1) 100%
+  );
+}
+
+.landing-gradient {
+  background: linear-gradient(
+    180deg,
+    rgba(53, 59, 130, 1) 0%,
+    rgba(211, 213, 228, 1) 60%,
+    rgba(253, 253, 254, 1) 100%
+  ) !important;
+}
+</style>
