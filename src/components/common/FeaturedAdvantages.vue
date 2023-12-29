adea27e8f91edcfebc4e3c6f94806beb434e3c6c
Ali-Rooholamini <ali.rooholamini.dev@gmail.com>
2023-12-29 21:41:05 +0330

    feat: add featured-advantage component

---
+++ src/components/common/FeaturedAdvantages.vue
@@ -0,0 +1,108 @@
+<template>
+  <div
+    class="featured-advantages justify-content-around justify-content-md-between"
+  >
+    <div
+      class="featured-advantages_feature"
+      v-for="(feature, index) in featuredList"
+      :key="index"
+    >
+      <div class="featured-advantages_feature_icon">
+        <nuxt-icon :name="'features/' + feature.iconName" />
+      </div>
+      <p class="featured-advantages_feature_explain">{{ feature.explain }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FeaturedAdvantages",
+
+  props: {
+    featuredList: {
+      type: [Array, Object],
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.featured-advantages {
+  display: flex;
+  align-items: flex-start;
+  gap: 15px;
+  width: 100%;
+  flex-wrap: wrap;
+}
+
+.featured-advantages_feature {
+  width: 90px;
+
+  @include breakpoint-up(md) {
+    width: 120px;
+  }
+
+  .featured-advantages_feature_icon {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 90px;
+    height: 90px;
+    border-radius: 50%;
+    box-shadow: 0px 25px 50px -5px rgba(0, 0, 0, 0.25);
+    cursor: pointer;
+
+    @include breakpoint-up(md) {
+      width: 120px;
+      height: 120px;
+    }
+
+    .nuxt-icon :deep(svg) {
+      width: 30px;
+      height: 30px;
+
+      @include breakpoint-up(md) {
+        width: 40px;
+        height: 40px;
+      }
+    }
+
+    &::before {
+      content: "";
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      z-index: -1;
+      background: linear-gradient(
+        180deg,
+        #f9d9e6 0%,
+        rgba(249, 217, 230, 0) 77.08%
+      );
+      opacity: 0;
+      transition: opacity 200ms linear;
+    }
+
+    &:hover::before {
+      opacity: 1;
+    }
+  }
+
+  .featured-advantages_feature_explain {
+    margin-top: 20px;
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 20.43px;
+    text-align: center;
+
+    @include breakpoint-up(md) {
+      margin-top: 40px;
+      font-size: 18px;
+      line-height: 22.43px;
+    }
+  }
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,105 @@
 <template>
-  <div class="mb-5">
-    <LandingHeader />
+  <div>
+    <section class="landing_header">
+      <LandingHeader />
+    </section>
+    <section class="container landing_why-vitamol">
+      <h1>چرا <span>ویتـــــامـول</span> ؟</h1>
+      <FeaturedAdvantages :featuredList="featuredAdvantagesList" />
+    </section>
   </div>
 </template>
 
 <script>
 import LandingHeader from "~/components/page/index/LandingHeader.vue";
+import FeaturedAdvantages from "~/components/common/FeaturedAdvantages.vue";
+
 export default {
   name: "IndexPage",
   components: {
     LandingHeader,
+    FeaturedAdvantages,
+  },
+  computed: {
+    featuredAdvantagesList() {
+      return [
+        {
+          iconName: "vit--star-circle",
+          explain: "تولید داخلی با مواد اولیه و فرمولاسیون ایتالیایی",
+        },
+        {
+          iconName: "vit--map-point-wave",
+          explain: "دارای بیش از 1500 فروشگاه فعال در سطح کشور",
+        },
+        {
+          iconName: "vit--tag-price",
+          explain: "قیمت مناسب نسبت به محصولات هم سطح",
+        },
+        {
+          iconName: "vit--hand-stars",
+          explain: "اعطای نمایندگی به واجدین شرایط در سراسر کشور",
+        },
+        {
+          iconName: "vit--bill",
+          explain: "میکرو پیگمنت آمریکایی",
+        },
+        {
+          iconName: "vit--cup-star",
+          explain: "تولید داخلی با مواد اولیه و فرمولاسیون ایتالیایی",
+        },
+      ];
+    },
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.landing_header {
+  margin-bottom: 40px;
+
+  @include breakpoint-up(md) {
+    margin-bottom: 65px;
+  }
+  @include breakpoint-up(lg) {
+    margin-bottom: 95px;
+  }
+}
+
+.landing_why-vitamol {
+  color: var(--secondary-color);
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-bottom: 60px;
+
+  @include breakpoint-up(md) {
+    margin-bottom: 120px;
+  }
+
+  h1 {
+    font-size: 37px;
+    margin-bottom: 66px;
+
+    @include breakpoint-up(sm) {
+      font-size: 47px;
+    }
+
+    @include breakpoint-up(md) {
+      font-size: 57px;
+    }
+
+    @include breakpoint-up(lg) {
+      font-size: 67px;
+    }
+
+    > span {
+      display: inline-block;
+      font-family: PeydaWeb;
+      padding: 0px 10px;
+      font-weight: bold;
+    }
+  }
+}
+</style>
